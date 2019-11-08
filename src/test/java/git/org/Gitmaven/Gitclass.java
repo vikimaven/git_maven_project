19be51d1e5032aa89a9ca5dbfa21d7488a560e79
vikimaven <vigneshvickii91@gmail.com>
2019-11-08 15:13:47 +0530

    git second commit

---
+++ src/test/java/git/org/Gitmaven/Gitclass.java
@@ -0,0 +1,20 @@
+package git.org.Gitmaven;
+
+public class Gitclass {
+	
+	public static void stameth()
+	{
+	System.out.println("static method");	
+	}
+	
+	public void Demo()
+	{
+		System.out.println("demo default constructor");
+		System.out.println("demo parameter constructor");
+	}
+	
+public static void main(String[] args) {
+	Gitclass obj = new Gitclass();
+	obj.Demo();
+}
+}
